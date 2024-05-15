eba79f7a33af989a28b3f862cbd1e5cd76539110
saad odeh <odehsaid46@gmail.com>
2024-05-15 17:11:02 +0200

    Dep

---
+++ Session2/Session2/Controllers/DepartmentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Session2.Data;
+using Session2.Models;
+
+namespace Session2.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        DepartmentContext context=new DepartmentContext();
+
+        public IActionResult Index()
+        {
+            var departments = context.Departments.ToList();
+            return View("Index",departments);
+        }
+        public IActionResult Details(int Id)
+        {
+            //return Content($"ID :{Id}");
+            var department = context.Departments.Find(Id);
+            return View("Details",department);
+        }
+        public IActionResult Create() { return View("Create"); }
+        public IActionResult Store(Department department)
+        {
+            context.Departments.Add(department);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int Id)
+        {
+            var department = context.Departments.Find(Id);
+            context.Departments.Remove(department);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+
+    }
+}
---
+++ Session2/Session2/Data/DepartmentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Session2.Models;
+
+namespace Session2.Data
+{
+    public class DepartmentContext :DbContext
+    {
+        public DbSet<Department> Departments { get; set; }  
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer("Server=db5045.public.databaseasp.net; Database=db5045; User Id=db5045; Password=t#9K-6XqE+a4; Encrypt=False; MultipleActiveResultSets=True;");
+        }
+    }
+}
